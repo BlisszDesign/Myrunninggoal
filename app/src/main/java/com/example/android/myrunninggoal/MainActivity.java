d579ba38f0b7f5c67b1d1232f30a9d56b82e10fe
Eszti <estera.tothova@gmail.com>
2018-02-24 17:52:07 +0100

    create new files, new styles, clean java code

--- app/src/main/java/com/example/android/myrunninggoal/MainActivity.java
+++ app/src/main/java/com/example/android/myrunninggoal/MainActivity.java
@@ -12,29 +12,70 @@ public class MainActivity extends AppCompatActivity {
 
     int totalKm = 0;
     int totalMin = 0;
-    int progress;
+    int progress, todayKm, todayMin;
     int goal = 100;
+    TextView progressView, kmView, minView, totalKmView, totalMinView;
+    static final String STATE_KM = "totalKm";
+    static final String STATE_MIN = "totalMin";
+    static final String STATE_PROGRESS = "progress";
+    static final String STATE_TODAY_KM = "todayKm";
+    static final String STATE_TODAY_MIN = "todayMin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kmView = findViewById(R.id.today_km);
+        minView = findViewById(R.id.today_min);
+        totalKmView = findViewById(R.id.total_km);
+        totalMinView = findViewById(R.id.total_min);
+        progressView = findViewById(R.id.goal_progress);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(STATE_TODAY_KM, todayKm);
+        savedInstanceState.putInt(STATE_TODAY_MIN, todayMin);
+        savedInstanceState.putInt(STATE_KM, totalKm);
+        savedInstanceState.putInt(STATE_MIN, totalMin);
+        savedInstanceState.putInt(STATE_PROGRESS, progress);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore state from saved instance
+        totalKm = savedInstanceState.getInt(STATE_KM);
+        totalMin = savedInstanceState.getInt(STATE_MIN);
+        progress = savedInstanceState.getInt(STATE_PROGRESS);
+        todayKm = savedInstanceState.getInt(STATE_TODAY_KM);
+        todayMin = savedInstanceState.getInt(STATE_TODAY_MIN);
+        displayTotalKm(totalKm);
+        displayTotalMin(totalMin);
+        displayTodayKm(todayKm);
+        displayTodayMin(todayMin);
+        displayProgress(progress);
     }
 
     //reset summary
     public void resetAll(View view) {
         totalKm = 0;
         totalMin = 0;
-        progress = 0;
+        progress = 100;
+        todayMin = 0;
+        todayKm = 0;
         displayTotalKm(totalKm);
         displayTotalMin(totalMin);
         displayProgress(progress);
+        displayTodayKm(todayKm);
+        displayTodayMin(todayMin);
         Toast.makeText(this, getString(R.string.toast_run), Toast.LENGTH_SHORT).show();
     }
 
     //set today km
     public void addFiveKm(View view) {
-        int todayKm = 5;
+        todayKm = 5;
         totalKm += todayKm;
         progress = goal - totalKm;
         if (progress < 0) {
@@ -43,11 +84,10 @@ public class MainActivity extends AppCompatActivity {
         displayTodayKm(todayKm);
         displayTotalKm(totalKm);
         displayProgress(progress);
-        todayKm = 0;
     }
 
     public void addTenKm(View view) {
-        int todayKm = 10;
+        todayKm = 10;
         totalKm += todayKm;
         progress = goal - totalKm;
         if (progress < 0) {
@@ -56,11 +96,10 @@ public class MainActivity extends AppCompatActivity {
         displayTodayKm(todayKm);
         displayTotalKm(totalKm);
         displayProgress(progress);
-        todayKm = 0;
     }
 
     public void addTwentyKm(View view) {
-        int todayKm = 20;
+        todayKm = 20;
         totalKm += todayKm;
         progress = goal - totalKm;
         if (progress < 0) {
@@ -69,61 +108,52 @@ public class MainActivity extends AppCompatActivity {
         displayTodayKm(todayKm);
         displayTotalKm(totalKm);
         displayProgress(progress);
-        todayKm = 0;
     }
 
     //    set today minutes
     public void addFiveMin(View view) {
-        int todayMin = 5;
+        todayMin = 5;
         totalMin += todayMin;
         displayTotalMin(totalMin);
         displayTodayMin(todayMin);
-        todayMin = 0;
     }
 
     public void addTenMin(View view) {
-        int todayMin = 10;
+        todayMin = 10;
         totalMin += todayMin;
         displayTotalMin(totalMin);
         displayTodayMin(todayMin);
-        todayMin = 0;
     }
 
     public void addThirtyMin(View view) {
-        int todayMin = 30;
+        todayMin = 30;
         totalMin += todayMin;
         displayTotalMin(totalMin);
         displayTodayMin(todayMin);
-        todayMin = 0;
     }
 
 
     //display today running km
     public void displayTodayKm(int km) {
-        TextView kmView = (TextView) findViewById(R.id.today_km);
         kmView.setText(String.valueOf(km));
     }
 
     //    display today running minutes
     public void displayTodayMin(int min) {
-        TextView minView = (TextView) findViewById(R.id.today_min);
         minView.setText(String.valueOf(min));
     }
 
     //    display total running km
     public void displayTotalKm(int km) {
-        TextView kmView = (TextView) findViewById(R.id.total_km);
-        kmView.setText(String.valueOf(km));
+        totalKmView.setText(String.valueOf(km));
     }
 
     //    display total running minutes
     public void displayTotalMin(int min) {
-        TextView minView = (TextView) findViewById(R.id.total_min);
-        minView.setText(String.valueOf(min));
+        totalMinView.setText(String.valueOf(min));
     }
 
-    public void displayProgress (int goalProgress) {
-        TextView progressView = (TextView) findViewById(R.id.goal_progress);
+    public void displayProgress(int goalProgress) {
         progressView.setText("" + goalProgress);
         if (goalProgress <= 25 && goalProgress > 0) {
             Toast.makeText(this, getString(R.string.toast_75), Toast.LENGTH_SHORT).show();
@@ -134,13 +164,13 @@ public class MainActivity extends AppCompatActivity {
         } else if (goalProgress <= 75 && goalProgress > 50) {
             Toast.makeText(this, getString(R.string.toast_25), Toast.LENGTH_SHORT).show();
             return;
-        }else if (totalKm == 100) {
+        } else if (totalKm == 100) {
             Toast.makeText(this, getString(R.string.toast), Toast.LENGTH_SHORT).show();
             return;
-        }else if (goalProgress < 100 && goalProgress >= 5){
+        } else if (goalProgress < 100 && goalProgress >= 5) {
             Toast.makeText(this, getString(R.string.toast_95), Toast.LENGTH_SHORT).show();
             return;
-        }else {
+        } else {
             Toast.makeText(this, getString(R.string.toast_run), Toast.LENGTH_SHORT).show();
             return;
         }
